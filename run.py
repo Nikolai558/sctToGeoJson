55b7884a74f10772e34f14fa14420e2592d5bffc
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2022-01-03 18:16:30 -0700

    Made changes

--- run.py
+++ run.py
@@ -6,10 +6,14 @@ from os.path import isfile, join
 class ConvertToGeojson:
 
     def __init__(self):
-        pass
+        self.write_file = True
+        self.sct_file_dir = "SctFiles"
+        self.altitude = "altitudes.json"
+        self.geo_json = None
+        self.run_geojson()
 
     def run_geojson(self):
-        self.write_geojson(self.get_file_names("SectorTxtFiles"))
+        self.write_geojson(self.get_file_names())
 
     def read_sector_file(self, file_path: str) -> list:
         output = []
@@ -31,51 +35,64 @@ class ConvertToGeojson:
         if self.check_list(output):
             return output
         else:
-            print("First Does not match last in:", file_path)
-            #output.append(output[0])
-            return [[-108.369140625,33.137551192346145],
-                     [-107.9736328125,32.287132632616384],
-                     [-107.0068359375,33.19273094190692],
-                     [-108.369140625,33.137551192346145]]
+            print(f"{file_path}\n\tThis File has coordinates that are incorrect."
+                  f"\n\tThe first and last coordinates have to be the same.")
+            return [[]]
 
-    @staticmethod
-    def check_list(coord_list: list) -> bool:
-        first = coord_list[0]
-        last = coord_list[-1]
-        if first == last:
-            return True
-
-    def write_geojson(self, directory: list, write_file: bool = True) -> dict:
+    def write_geojson(self, directory: list) -> dict:
         output = {"type": "FeatureCollection",
                   "features": []
                   }
 
         for file in directory:
-            output["features"].append({"type": "Feature",
-                                       "properties": self.get_properties(file),
-                                       "geometry": {"type": "Polygon",
-                                                    "coordinates": [self.read_sector_file(file)]},
-                                       })
+            add_feature = {"type": "Feature",
+                           "properties": self.get_properties(file),
+                           "geometry": {"type": "Polygon",
+                                        "coordinates": [self.read_sector_file(file)]
+                                        }
+                           }
+            if add_feature["geometry"]["coordinates"] != [[[]]]:
+                output["features"].append(add_feature)
 
-        if write_file:
+        if self.write_file:
             with open("data.json", "w") as outfile:
                 json.dump(output, outfile, indent=4)
+        self.geo_json = output
 
         return output
 
+    def get_file_names(self) -> list:
+        only_files = []
+        for file in listdir(self.sct_file_dir):
+            if isfile(join(self.sct_file_dir, file)):
+                only_files.append(join(self.sct_file_dir, file))
+
+        return only_files
+
     def get_properties(self, filename) -> dict:
-        output = {"file_path": filename,
-                  "id": "-".join(filename.split('\\')[1].replace(".txt", "").split('_')[1:]),
-                  "alt-low": "",
-                  "alt-high": "",
-                  "alt-range": "",
+        sector_id = "-".join(filename.split('\\')[1].replace(".txt", "").split('_')[1:])
+        alt_high = ""
+        alt_low = ""
+
+        try:
+            with open(self.altitude, "r") as file:
+                artcc_sector_altitudes = json.load(file)
+                alt_high = artcc_sector_altitudes[sector_id.split('-')[0]][sector_id]["high"]
+                alt_low = artcc_sector_altitudes[sector_id.split('-')[0]][sector_id]["low"]
+        except KeyError as e:
+            print(f"Key Error: Can NOT assign ALT limits for {e}")
+        except FileNotFoundError as e:
+            print(f"File Not Found Error: {e}")
+            exit(1)
+
+        output = {"id": sector_id,
+                  "alt-low": alt_low,
+                  "alt-high": alt_high,
                   "stroke": "#555555",
                   "stroke-width": 1,
                   "stroke-opacity": 1,
                   "fill": "#940000",
-                  "fill-opacity": "0.5",
-                  "height": 20,
-                  "elevation_m": 60000}
+                  "fill-opacity": "0.5"}
 
         return output
 
@@ -95,16 +112,12 @@ class ConvertToGeojson:
         return latitude, longitude
 
     @staticmethod
-    def get_file_names(directory: str) -> list:
-        only_files = []
-        for file in listdir(directory):
-            if isfile(join(directory, file)):
-                only_files.append(join(directory, file))
-
-        return only_files
+    def check_list(coord_list: list) -> bool:
+        first = coord_list[0]
+        last = coord_list[-1]
+        if first == last:
+            return True
 
 
 if __name__ == '__main__':
     obj = ConvertToGeojson()
-    obj.run_geojson()
-    #obj.write_kml(json_file_path="C:\\Users\\Pocono Coast West\\Dropbox\\ZLC 4. Facility Engineer\\05_OTHER\\ZLC_Sector_Data.json")
